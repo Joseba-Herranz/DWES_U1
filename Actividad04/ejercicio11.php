b37ad6d30b63afd745e3a82d0426ec06fce11960
2daw3 <2daw3@HZ114496.H012108.NET>
2022-09-22 10:09:17 +0200

    11-12

--- Actividad04/ejercicio11.php
+++ Actividad04/ejercicio11.php
@@ -5,39 +5,31 @@ En este programa tendrás que utilizar un array asociativo para guardar como key
 la semana y asociar a cada día un valor entero. Se trandrá que mostrar en pantalla cada día y 
 valor almacenado en el array así como la suma y media de todos los valores guadados en el array.
 */
+$suma=0;
+$media=0;
 
 $semana = [
-    [
-        'Dia' => 'Lunes',
-        'Numero' => '1',
-    ],
-    [
-        'Dia' => 'Martes',
-        'Numero' => '2',
-    ],
-    [
-        'Dia' => 'Miercoles',
-        'Numero' => '3',
-    ],
-    [
-        'Dia' => 'Jueves',
-        'Numero' => '4',
-    ],
-    [
-        'Dia' => 'Viernes',
-        'Numero' => '5',
-    ],
-    [
-        'Dia' => 'Sabado',
-        'Numero' => '6',
-    ],
-    [
-        'Dia' => 'Domingo',
-        'Numero' => '7',
-    ]
+    
+        'Lunes' => 1,
+        'Martes' => 2,
+        'Miercoles' => 3,
+        'Jueves' => 4,
+        'Viernes' => 5,
+        'Sabado' => 6,
+        'Domingo' => 7
+    
 ];
 
 print_r($semana);
 
+foreach ($semana as $key => $valor){
+    $suma = $suma + $valor;
+}
+
+$media = $suma/7;
+
 echo '</br>';
+echo 'Suma: ' . $suma;
+echo '</br>';
+echo 'Media: ' . $media;
 ?>
---
+++ Actividad04/ejercicio12.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Ejercicio 12
+Crea un array asociativo y ordenalo dependiendo del key y del valor. Para ello has de 
+utilizar las funciones ksort y natsort.
+*/
+
+$asociativo = [
+    'emilio' => 'Jordi Rubio',
+    'marta' => 'Marta Fabra',
+    'eva' => 'Eva Palacio',
+    'toni' => 'Antonio Comas',
+    'emilio' => 'Emilio Morales',
+    'fco' => 'Francisco Cascales'
+];
+echo '<strong>Mostrar el array</strong></br>';
+foreach($asociativo as $key => $val){
+    echo "$key = $val </br>";
+}
+echo '</br>';
+echo '<strong>Ordenar por clave</strong></br>';
+ksort($asociativo);
+
+foreach($asociativo as $key => $val){
+    echo "$key = $val </br>";
+}
+echo '</br>';
+echo '<strong>Ordenar por contenido</strong></br>';
+natsort($asociativo);
+
+foreach($asociativo as $key => $val){
+    echo "$key = $val </br>";
+}
+?>
