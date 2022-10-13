396954885bc57b858481e37e6ee87968e8b57882
2daw3 <2daw3@HZ114496.H012108.NET>
2022-10-13 09:40:13 +0200

    update

--- Actividad08/dashboard.php
+++ Actividad08/dashboard.php
@@ -8,10 +8,10 @@ if(!isset($_SESSION['user'])){
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8">ç
+		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE-edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Document</title>
+		<title>Documento</title>
 	</head>
 	<body>
 		<h1>Dashboard</h1>
